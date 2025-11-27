66ab69457fe6d37bc5f10b7b629da4ab83b53ee1
kingkiboots <tuffhr4@gmail.com>
2025-11-27 23:06:38 +0900

    Feat: 푸터 구조만 추가

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,7 +1,21 @@
 import React, { memo } from "react";
 
 export const Footer = memo(() => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-background grid grid-cols-2 items-center gap-4 px-8 py-20">
+      <div className="flex">
+        <div>github</div>
+        <div>linkedin</div>
+        <div>t-story</div>
+      </div>
+      <div>
+        Loosely Designed in <em>Freeform</em> and coded in <em>Cursor</em> by
+        yours truly. Build with <em>Next.js</em> and <em>Tailwind CSS</em>,
+        deployed with []. All text is set in the
+        <em>Inter, Pretendard</em> typfaces.
+      </div>
+    </footer>
+  );
 });
 
 Footer.displayName = "Footer";
