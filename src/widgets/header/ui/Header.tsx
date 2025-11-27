ecebc3bffce1c2f766f5cefb572412210895a56f
kingkiboots <tuffhr4@gmail.com>
2025-11-27 23:07:19 +0900

    Feat: 헤더 스티키 기능과 bgcolor 추가

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,15 +3,12 @@ import React, { memo } from "react";
 
 export const Header = memo(() => {
   return (
-    <header className="sm:display-none relative flex h-24 items-center justify-between px-8">
+    <header className="sm:display-none bg-background sticky top-0 flex h-24 items-center justify-between px-8">
       <Link href="/">Logo</Link>
       <nav className="hidden items-center gap-x-12 lg:flex">
         <ul className="flex items-center gap-12">
           <li>
-            <Link
-              className="hover:text-text-tertiary rounded px-6 py-3"
-              href="#"
-            >
+            <Link className="hover:text-tertiary rounded px-6 py-3" href="#">
               About
             </Link>
           </li>
@@ -33,7 +30,7 @@ export const Header = memo(() => {
           <li>
             <button
               type="button"
-              className="btn bg-primary-500 text-text-primary cursor-pointer rounded px-6 py-3 font-bold"
+              className="btn bg-primary text-text-primary cursor-pointer rounded px-6 py-3 font-bold"
             >
               Resume
             </button>
