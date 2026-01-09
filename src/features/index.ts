a7ee76c3260e58fb73aab40de35c7397474b04d6
kingkiboots <tuffhr4@gmail.com>
2026-01-09 22:49:24 +0900

    feat: 메인 페이지 조합 - 모든 섹션 통합

---
+++ src/features/index.ts
@@ -0,0 +1,6 @@
+// Features exports
+export { HeroSection } from "./hero";
+export { SkillsSection } from "./skills";
+export { ProjectsSection } from "./projects";
+export { CareerSection } from "./career";
+
